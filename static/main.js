c243af229cc13f73df47e1eb9317898018bdc4bd
chouchane.louai20@gmail.com <chouchane.louai20@gmail.com>
2026-04-26 19:59:29 +0100

    <feat>: copy to clipboard onclick implemented

--- static/main.js
+++ static/main.js
@@ -1,4 +1,10 @@
 const deleteLinks = document.querySelectorAll(".btn-delete")
+const linkTexts = document.querySelectorAll(".in-link")
+
+let toast = new Notyf({
+    position: {x: "center", y: "top"},
+    dismissibleL: true
+})
 
 document.addEventListener("DOMContentLoaded", () => {
     deleteLinks.forEach(link => {
@@ -8,8 +14,25 @@ document.addEventListener("DOMContentLoaded", () => {
             await fetch(`${e.currentTarget.href}`, {
                 method: "DELETE"
             })
+            toast.success(`Link deleted`)
+            setTimeout(() => {
+                history.go(0)
 
-            history.go(0)
+            }, 1500)
+
+        })
+    })
+
+    linkTexts.forEach(link => {
+        link.addEventListener("click", async (e) => {
+            e.preventDefault()
+            try {
+                await navigator.clipboard.writeText(e.target.textContent)
+                toast.success(`Link is copied to clipboard`)
+            } catch (error) {
+                console.error(error);
+                toast.success(`Failed to copy link to clipboard`)
+            }
         })
     })
 })
